efb75b3a5fd7626e9f49c9c6a01c70e74c7c3793
lucasmonstrox <joao.galiano.silva@gmail.com>
2024-06-28 19:28:46 +0100

    refactor: improve clean code

--- benches/battle_list.rs
+++ benches/battle_list.rs
@@ -2,20 +2,20 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use rustibia::client::battle_list::BattleList;
 use rustibia::utils::image::load_image;
 
-fn benchmark_get_creatures_count_when_no_creatures(c: &mut Criterion) {
+fn benchmark_get_creatures_count_when_there_are_no_creatures(c: &mut Criterion) {
     let content = load_image("./assets/examples/client/battle_list/no_creatures.png").unwrap();
     let battle_list = BattleList { content };
 
-    c.bench_function("get_creatures_count_when_no_creatures", |b| {
+    c.bench_function("get_creatures_count_when_there_are_no_creatures", |b| {
         b.iter(|| battle_list.get_creatures_count())
     });
 }
 
-fn benchmark_get_creatures_count_when_full_of_creatures(c: &mut Criterion) {
+fn benchmark_get_creatures_count_when_its_full_of_creatures(c: &mut Criterion) {
     let content = load_image("./assets/examples/client/battle_list/full_of_creatures.png").unwrap();
     let battle_list = BattleList { content };
 
-    c.bench_function("get_creatures_count_when_full_of_creatures", |b| {
+    c.bench_function("get_creatures_count_when_its_full_of_creatures", |b| {
         b.iter(|| battle_list.get_creatures_count())
     });
 }
@@ -48,36 +48,36 @@ fn benchmark_has_creature_in_target_when_has_no_creature_in_target(c: &mut Crite
     );
 }
 
-fn benchmark_has_creature_in_target_when_has_creature_target_in_first_position(c: &mut Criterion) {
+fn benchmark_has_creature_in_target_when_target_is_in_first_position(c: &mut Criterion) {
     let content =
-        load_image("./assets/examples/client/battle_list/target_in_first_position.png").unwrap();
+        load_image("./assets/examples/client/battle_list/target_is_in_first_position.png").unwrap();
     let battle_list = BattleList { content };
 
     c.bench_function(
-        "has_creature_in_target_when_has_creature_target_in_first_position",
+        "has_creature_in_target_when_target_is_in_first_position",
         |b| b.iter(|| battle_list.has_creature_in_target()),
     );
 }
 
-fn benchmark_has_creature_in_target_when_has_creature_target_in_last_position(c: &mut Criterion) {
+fn benchmark_has_creature_in_target_when_target_is_in_last_position(c: &mut Criterion) {
     let content =
-        load_image("./assets/examples/client/battle_list/target_in_first_position.png").unwrap();
+        load_image("./assets/examples/client/battle_list/target_is_in_last_position.png").unwrap();
     let battle_list = BattleList { content };
 
     c.bench_function(
-        "has_creature_in_target_when_has_creature_target_in_last_position",
+        "has_creature_in_target_when_target_is_in_last_position",
         |b| b.iter(|| battle_list.has_creature_in_target()),
     );
 }
 
 criterion_group!(
     benches,
-    benchmark_get_creatures_count_when_no_creatures,
-    benchmark_get_creatures_count_when_full_of_creatures,
+    benchmark_get_creatures_count_when_there_are_no_creatures,
+    benchmark_get_creatures_count_when_its_full_of_creatures,
     benchmark_get_max_creatures_count,
     benchmark_has_creature_in_target_when_there_are_no_creatures,
     benchmark_has_creature_in_target_when_has_no_creature_in_target,
-    benchmark_has_creature_in_target_when_has_creature_target_in_first_position,
-    benchmark_has_creature_in_target_when_has_creature_target_in_last_position
+    benchmark_has_creature_in_target_when_target_is_in_first_position,
+    benchmark_has_creature_in_target_when_target_is_in_last_position
 );
 criterion_main!(benches);
--- tests/battle_list.rs
+++ tests/battle_list.rs
@@ -2,52 +2,52 @@ use rustibia::client::battle_list::BattleList;
 use rustibia::utils::image::load_image;
 
 #[test]
-fn should_get_creatures_count_return_0_when_there_are_no_creatures() {
+fn should_method_get_creatures_count_return_0_when_there_are_no_creatures() {
     let content = load_image("./assets/examples/client/battle_list/no_creatures.png").unwrap();
     let battle_list = BattleList { content };
     assert_eq!(battle_list.get_creatures_count(), 0);
 }
 
 #[test]
-fn should_get_creatures_count_return_45_when_its_full_of_creatures() {
+fn should_method_get_creatures_count_return_45_when_its_full_of_creatures() {
     let content = load_image("./assets/examples/client/battle_list/full_of_creatures.png").unwrap();
     let battle_list = BattleList { content };
     assert_eq!(battle_list.get_creatures_count(), 45);
 }
 
 #[test]
-fn should_get_max_creatures_count_return_45() {
+fn should_method_get_max_creatures_count_return_45() {
     let content = load_image("./assets/examples/client/battle_list/full_of_creatures.png").unwrap();
     let battle_list = BattleList { content };
     assert_eq!(battle_list.get_max_creatures_count(), 45);
 }
 
 #[test]
-fn should_has_creature_in_target_return_false_when_there_are_no_creatures() {
+fn should_method_has_creature_in_target_return_false_when_there_are_no_creatures() {
     let content = load_image("./assets/examples/client/battle_list/no_creatures.png").unwrap();
     let battle_list = BattleList { content };
     assert_eq!(battle_list.has_creature_in_target(), false);
 }
 
 #[test]
-fn should_has_creature_in_target_return_false_when_has_no_creature_in_target() {
+fn should_method_has_creature_in_target_return_false_when_has_no_creature_in_target() {
     let content = load_image("./assets/examples/client/battle_list/full_of_creatures.png").unwrap();
     let battle_list = BattleList { content };
     assert_eq!(battle_list.has_creature_in_target(), false);
 }
 
 #[test]
-fn should_has_creature_in_target_return_true_when_has_creature_target_in_first_position() {
+fn should_method_has_creature_in_target_return_true_when_target_is_in_first_position() {
     let content =
-        load_image("./assets/examples/client/battle_list/target_in_first_position.png").unwrap();
+        load_image("./assets/examples/client/battle_list/target_is_in_first_position.png").unwrap();
     let battle_list = BattleList { content };
     assert_eq!(battle_list.has_creature_in_target(), true);
 }
 
 #[test]
-fn should_has_creature_in_target_return_true_when_has_creature_target_in_last_position() {
+fn should_method_has_creature_in_target_return_true_when_target_is_in_last_position() {
     let content =
-        load_image("./assets/examples/client/battle_list/target_in_last_position.png").unwrap();
+        load_image("./assets/examples/client/battle_list/target_is_in_last_position.png").unwrap();
     let battle_list = BattleList { content };
     assert_eq!(battle_list.has_creature_in_target(), true);
 }
