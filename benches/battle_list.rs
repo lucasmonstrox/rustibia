697f738f11a9ebb3b237cefeea3c09cf9403dc9b
lucasmonstrox <joao.galiano.silva@gmail.com>
2024-06-28 17:49:33 +0100

    perf: add benchmarks to target

--- benches/battle_list.rs
+++ benches/battle_list.rs
@@ -1,20 +1,27 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use ndarray::ArrayView3;
 use rustibia::client::battle_list::BattleList;
+use rustibia::utils::image::load_image;
 
-fn benchmark_get_creatures_count(c: &mut Criterion) {
-    let content_vec = image::open("content.png").unwrap().as_bytes().to_vec();
-    let content = ArrayView3::from_shape((998, 156, 4), &content_vec).unwrap();
+fn benchmark_get_creatures_count_empty(c: &mut Criterion) {
+    let content = load_image("./assets/examples/client/battle_list/empty_battle_list.png").unwrap();
     let battle_list = BattleList { content };
 
-    c.bench_function("get_creatures_count", |b| {
+    c.bench_function("get_creatures_count_empty", |b| {
+        b.iter(|| battle_list.get_creatures_count())
+    });
+}
+
+fn benchmark_get_creatures_count_full(c: &mut Criterion) {
+    let content = load_image("./assets/examples/client/battle_list/full_battle_list.png").unwrap();
+    let battle_list = BattleList { content };
+
+    c.bench_function("get_creatures_count_full", |b| {
         b.iter(|| battle_list.get_creatures_count())
     });
 }
 
 fn benchmark_get_max_creatures_count(c: &mut Criterion) {
-    let content_vec = image::open("content.png").unwrap().as_bytes().to_vec();
-    let content = ArrayView3::from_shape((998, 156, 4), &content_vec).unwrap();
+    let content = load_image("./assets/examples/client/battle_list/full_battle_list.png").unwrap();
     let battle_list = BattleList { content };
 
     c.bench_function("get_max_creatures_count", |b| {
@@ -22,20 +29,30 @@ fn benchmark_get_max_creatures_count(c: &mut Criterion) {
     });
 }
 
-fn benchmark_has_creature_in_target(c: &mut Criterion) {
-    let content_vec = image::open("content.png").unwrap().as_bytes().to_vec();
-    let content = ArrayView3::from_shape((998, 156, 4), &content_vec).unwrap();
+fn benchmark_has_creature_in_target_first(c: &mut Criterion) {
+    let content = load_image("./assets/examples/client/battle_list/target_first.png").unwrap();
     let battle_list = BattleList { content };
 
-    c.bench_function("has_creature_in_target", |b| {
+    c.bench_function("has_creature_in_target_first", |b| {
+        b.iter(|| battle_list.has_creature_in_target())
+    });
+}
+
+fn benchmark_has_creature_in_target_last(c: &mut Criterion) {
+    let content = load_image("./assets/examples/client/battle_list/target_last.png").unwrap();
+    let battle_list = BattleList { content };
+
+    c.bench_function("has_creature_in_target_last", |b| {
         b.iter(|| battle_list.has_creature_in_target())
     });
 }
 
 criterion_group!(
     benches,
-    benchmark_get_creatures_count,
+    benchmark_get_creatures_count_empty,
+    benchmark_get_creatures_count_full,
     benchmark_get_max_creatures_count,
-    benchmark_has_creature_in_target
+    benchmark_has_creature_in_target_first,
+    benchmark_has_creature_in_target_last
 );
 criterion_main!(benches);
