8d515b3c6b2aec451f31c602b6b4ade4e87e2b43
lucasmonstrox <joao.galiano.silva@gmail.com>
2024-07-01 11:00:49 +0100

    refactor: add 1 percent pixel color

--- src/client/status_bar/constants.rs
+++ src/client/status_bar/constants.rs
@@ -1,6 +1,6 @@
 // HP_PERCENTAGE_MAPPER maps pixel coordinates (x, y) on the lifebar and their RGB colors to corresponding HP percentages.
 // [([usize; 2], (u8, u8, u8), u32); 11] format ensures each tuple holds lifebar pixel coordinates, expected color, and the resulting HP value.
-pub const HP_PERCENTAGE_MAPPER: [([usize; 2], (u8, u8, u8), u32); 11] = [
+pub const HP_PERCENTAGE_MAPPER: [([usize; 2], (u8, u8, u8), u32); 12] = [
     ([5, 107], (100, 46, 49), 100),
     ([5, 98], (219, 79, 79), 90),
     ([5, 88], (219, 79, 79), 80),
@@ -12,11 +12,12 @@ pub const HP_PERCENTAGE_MAPPER: [([usize; 2], (u8, u8, u8), u32); 11] = [
     ([5, 32], (219, 79, 79), 20),
     ([5, 22], (219, 79, 79), 10),
     ([5, 18], (219, 79, 79), 5),
+    ([5, 14], (120, 61, 64), 1),
 ];
 
 // MANA_PERCENTAGE_MAPPER maps pixel coordinates (x, y) on the manabar and their RGB colors to corresponding Mana percentages.
 // [([usize; 2], (u8, u8, u8), u32); 11] format ensures each tuple holds lifebar pixel coordinates, expected color, and the resulting Mana value.
-pub const MANA_PERCENTAGE_MAPPER: [([usize; 2], (u8, u8, u8), u32); 11] = [
+pub const MANA_PERCENTAGE_MAPPER: [([usize; 2], (u8, u8, u8), u32); 12] = [
     ([18, 107], (45, 45, 105), 100),
     ([18, 98], (83, 80, 218), 90),
     ([18, 88], (83, 80, 218), 80),
@@ -28,4 +29,5 @@ pub const MANA_PERCENTAGE_MAPPER: [([usize; 2], (u8, u8, u8), u32); 11] = [
     ([18, 32], (83, 80, 218), 20),
     ([18, 22], (83, 80, 218), 10),
     ([18, 18], (83, 80, 218), 5),
+    ([18, 14], (61, 61, 125), 1),
 ];
