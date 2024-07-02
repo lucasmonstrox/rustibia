c121ab52850121af410671f69909061fddf04078
lucasmonstrox <joao.galiano.silva@gmail.com>
2024-07-02 11:08:37 +0100

    fix: add correct path name

--- tests/action_bars.rs
+++ tests/action_bars.rs
@@ -3,14 +3,14 @@ use rustibia::utils::image::load_image;
 
 #[test]
 fn should_method_is_equipped_return_false_when_action_is_empty() {
-    let content = load_image("./assets/examples/client/actions_bar/action/empty.png").unwrap();
+    let content = load_image("./assets/examples/client/action_bars/action/empty.png").unwrap();
     let action = Action { content };
     assert_eq!(action.is_equipped(), false);
 }
 
 #[test]
 fn should_method_is_equipped_return_true_when_action_is_equipped() {
-    let content = load_image("./assets/examples/client/actions_bar/action/equipped.png").unwrap();
+    let content = load_image("./assets/examples/client/action_bars/action/equipped.png").unwrap();
     let action = Action { content };
     assert_eq!(action.is_equipped(), true);
 }
